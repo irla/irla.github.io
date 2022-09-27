6a16afa98fffc66658b421c0a788f87de4611ee4
irla <irla.pawel@gmail.com>
2022-09-27 06:58:34 +0200

    Some skills updates and stars fix

--- components/Score.tsx
+++ components/Score.tsx
@@ -3,16 +3,19 @@ import { StarIcon as SolidStar } from '@heroicons/react/24/solid'
 
 interface ScoreProps {
     max: number,
-    current?: number,
+    current: number,
 }
 
 export const Score: React.FC<ScoreProps> = ({max, current}) => {
     let stars: JSX.Element[] = [];
     if (current) {
-        for (let i = 0; i < current; i++) {
+        const total = 5
+        const full = Math.floor(current / max * 5)
+        const empty = total - full
+        for (let i = 0; i < full; i++) {
             stars.push(<SolidStar className="inline w-4 h-4 mr-1" key={'star-' + i}/>)
         }
-        for (let i = current; i < max; i++) {
+        for (let i = full; i < total; i++) {
             stars.push(<EmtpyStar className="inline w-4 h-4 mr-1" key={'star-' + i}/>)
         }
 
